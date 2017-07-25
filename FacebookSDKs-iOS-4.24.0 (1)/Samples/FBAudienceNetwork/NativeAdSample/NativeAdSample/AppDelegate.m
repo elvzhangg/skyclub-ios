5c1361feb4a78e93456e293f34a2bd71e68b3bbe
Guofeng Shi <guofengshi1999@gmail.com>
2017-07-25 11:06:38 +0800

    facebook login update

--- FacebookSDKs-iOS-4.24.0 (1)/Samples/FBAudienceNetwork/NativeAdSample/NativeAdSample/AppDelegate.m
+++ FacebookSDKs-iOS-4.24.0 (1)/Samples/FBAudienceNetwork/NativeAdSample/NativeAdSample/AppDelegate.m
@@ -20,15 +20,29 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  // Override point for customization after application launch.
-  self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-  self.window.rootViewController = self.viewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
+    // Add any custom logic here.
+    return YES;
 }
+    
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                  openURL:url
+                                                        sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                                               annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+                    ];
+    // Add any custom logic here.
+    return handled;
+}
+    
+    
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
