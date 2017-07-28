c7acd4b437974b652b30eff549fcf873d511188e
elvzhangg <elviszhang0606@gmail.com>
2017-07-28 16:13:44 +0800

    headercell updated

--- skyclub/HeaderCell.swift
+++ skyclub/HeaderCell.swift
@@ -14,6 +14,7 @@ class HeaderCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var sexLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
 
     
     override func awakeFromNib() {
